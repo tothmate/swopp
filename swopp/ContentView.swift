1b2740dcfb2698485ffd8ced142fa46ad4cecd6b
Akos Toth-Mate <akos@tothmate.com>
2022-02-02 14:57:12 +0100

    Initial Commit

---
+++ swopp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swopp
+//
+//  Created by Akos Toth-Mate on 2022. 02. 02..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swopp/swoppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swoppApp.swift
+//  swopp
+//
+//  Created by Akos Toth-Mate on 2022. 02. 02..
+//
+
+import SwiftUI
+
+@main
+struct swoppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
